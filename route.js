f8b5214610be9538e65887e6fca0c53d36abeed5
Andrey Sokolov <sokolov-as-i@yandex.ru>
2017-05-30 02:57:40 +0300

    added result sending

--- route.js
+++ route.js
@@ -1,13 +1,26 @@
 var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, process.env.STATIC_DIR + "up/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({ storage: storage });
 
 var uploadfunc = (req, res) => {
-    console.log(req.files);
-    console.log(req.body);
-    res.send('ok');
-}
+    console.log(req.file.filename);
+    var data = {
+        name: req.file.filename,
+        url: "/st/up/" + req.file.filename,
+        size: req.file.size
+    };
+    res.send(JSON.stringify(data));
+};
 
 module.exports = function(app) {
     app.get("/", (req, res) => {res.render("main");});
 
-    app.post("/upload", multer({dest: process.env.STATIC_DIR}).single('pic'), uploadfunc);
+    app.post("/upload", upload.single('pic'), uploadfunc);
 }
